62882ad295f077a71116f67b08ce03600b6bd9e1
Lennie Fehler <lenas.fehler@web.de>
2023-04-01 11:20:20 +0200

    score + appearance

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
   let jsonData = [];
   let guess = Number;
   let guessed = false;
+  let score = 0; 
 
  
   onMount(() => {
@@ -24,7 +25,6 @@
   function showData(json, index) {
     if (index <= 12) {
     data = json[index];
-    console.log(data);
     } else {
       fetchData();
       i = 0;
@@ -36,6 +36,24 @@
       guessed = false;
     } else {
       guessed = true;
+      let percent = guess/(data.price / 100);
+      console.log(percent);
+      let points = 0;
+
+      if (percent == 100) {
+        points = 10;
+      } else if (percent >= 90 && percent <= 110) {
+        points = 5;
+      } else if (percent >= 85 && percent <= 115) {
+        points = 4;
+      } else if (percent >= 80 && percent <= 120) {
+        points = 3
+      } else if (percent >= 75 && percent <= 125) {
+        points = 2;
+      } else if (percent >= 70 && percent <= 130) {
+        points = 1
+      } else {return}
+      score = score + points;
     }
   }
 
@@ -43,7 +61,7 @@
     i = i + 1;
     showData(jsonData, i);
     guessed = false;
-   guess = undefined;
+    guess = undefined;
   }
 
 </script>
@@ -55,19 +73,21 @@
 {:then}
   <h1 id="title">Guess the price</h1>
   {#if guessed}
-    <div class="mid">
       <h2>{data.name}</h2>
+      <h4>{data.description}</h4>
       <img src={data.imageUrl} alt={data.imageUrl} class="guessPicture" style="max-height: 300px; max-width: 500px">
       <h4>Your guess: {guess}</h4>
       <h4>Actual price: {data.price}</h4>
-    </div>
     <button class="click" on:click={() => onClickedNext()}>Next</button>
   {:else}
-    <div class="mid">
       <h2>{data.name}</h2>
+      <h4>{data.description}</h4>
       <img src={data.imageUrl} alt={data.imageUrl} class="guessPicture" style="max-height: 300px; max-width: 500px">
-      <input type="number" bind:value={guess}>
+      <input type="number" bind:value={guess}>    
       <button on:click={() => {onClickedGuess(jsonData)}}>Guess</button>
+      <div id="pointsDiv">
+        <h3>Points</h3>
+        <h4>{score}</h4>
     </div>
     <button class="click" on:click={() => onClickedNext()} >Next</button>
   {/if}
